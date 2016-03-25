eab97ad123c49e4acebd3df467ead809e0e45b89
Andreas Jung <yet@gmx.de>
2016-03-25 16:12:59 +0100

    updated

--- aj/generate-json.py
+++ aj/generate-json.py
@@ -23,24 +23,47 @@ with open(sys.argv[-1], 'rb') as fp:
         if row['Export'] != '1':
             continue
 
-        d = {
-            "description": row['Beschreibung'],
-            "name": row['Beschreibung'],
-            "services": [
-                    {
-                            "type": row['HomeKit Type'],
-                            "description": row['Beschreibung'],
-                            "name": row['HomeKit Name'],
-                            "On": {
-                                    "Set": row['GA Schalten'],
-                                    "Listen": [
-                                            row['GA RM']
-                                     ]
-                             }
-                     }
-            ],
-            "services-description": row['Beschreibung']
-        }
+        if row['HomeKit Type'] == 'Lightbulb':
+            d = {
+                "description": row['Beschreibung'],
+                "name": row['Beschreibung'],
+                "services": [
+                        {
+                                "type": row['HomeKit Type'],
+                                "description": row['Beschreibung'],
+                                "name": row['HomeKit Name'],
+                                "On": {
+                                        "Set": row['GA Schalten'],
+                                        "Listen": [
+                                                row['GA RM']
+                                         ]
+                                 }
+                         }
+                ],
+                "services-description": row['Beschreibung']
+            }
+        elif row['HomeKit Type'] == 'Temperature Sensor':
+            d = {
+                "description": row['Beschreibung'],
+                "name": row['Beschreibung'],
+                "services": [
+                        {
+                                "type": row['HomeKit Type'],
+                                "description": row['Beschreibung'],
+                                "name": row['HomeKit Name'],
+                                "CurrentTemperature": {
+                                        "Set": "",
+                                        "Listen": [
+                                                row['GA Schalten']
+                                         ]
+                                 }
+                         }
+                ],
+                "services-description": row['Beschreibung']
+            }
+        else:
+            continue
+
         print d
         accessories.append(d)
 
